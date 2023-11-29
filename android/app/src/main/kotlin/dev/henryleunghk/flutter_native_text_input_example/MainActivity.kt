4f9d834dbf5b332d2074b7e0ccee8a8903ade594
delfme <53510751+delfme@users.noreply.github.com>
2023-11-29 15:34:22 +0100

    Update MainActivity.kt

--- android/app/src/main/kotlin/dev/henryleunghk/flutter_native_text_input_example/MainActivity.kt
+++ android/app/src/main/kotlin/dev/henryleunghk/flutter_native_text_input_example/MainActivity.kt
@@ -3,4 +3,7 @@ package dev.henryleunghk.flutter_native_text_input_example
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+  override fun getRenderMode(): RenderMode {
+        return RenderMode.texture
+    }
 }
